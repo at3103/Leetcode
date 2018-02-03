38dcee4641c521d3564bbce41d40dc0a0f2a9156
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2018-02-02 23:31:22 -0800

    Added LeetCode 231,342

---
+++ Power of Two_231.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return bin(n).count('1') == 1 and n >=0
+        
